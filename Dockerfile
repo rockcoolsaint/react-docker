68e7b5e289c030403d2876c9c4f7f278ee37ce91
innocent <innocent@debian.Debian.org>
2022-07-24 00:44:39 +0100

    docker volumes and environment variables

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN npm install
 
 COPY . .
 
+ENV REACT_APP_NAME=myName
+
 EXPOSE 3000
 
 CMD ["npm", "start"]
--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello there</p>
+        <p>{`My name is ${process.env.REACT_APP_NAME}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
