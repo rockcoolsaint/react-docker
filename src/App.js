eb792990f26211019f8b3eeac0c2f3beef43ef3d
innocent <innocent@debian.Debian.org>
2022-07-29 19:38:46 +0100

    docker-compose for dev and production environments

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Hello there</p>
-        <p>{`My name is ${process.env.REACT_APP_NAME}`}</p>
+        <p>{`My name is ${process.env.REACT_APP_NAME} asdf`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
